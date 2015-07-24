0a9501e586a1d4ffecc0045d9e52edeb7aaca700
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-24 17:18:09 +0100

    Create Question1.m

---
+++ Lab0/Question1.m
@@ -0,0 +1,14 @@
+clear all
+
+x = 3;
+y = 2.5;
+
+z = x+y;
+w = x-y;
+q = w/z;
+r = q*q;
+
+fprintf(1, 'z = %f \n',z);
+fprintf(1, 'w = %f \n',w);
+fprintf(1, 'q = %f \n',q);
+fprintf(1, 'r = %f \n',r);
