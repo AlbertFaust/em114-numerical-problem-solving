5341dddf769ffd4866dbc14c80a411095b52315e
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-24 17:33:47 +0100

    Create Question2.m

---
+++ Lab0/Question2.m
@@ -0,0 +1,17 @@
+%Question1.m (https://github.com/AlbertFaust/em114-numerical-problem-solving/blob/master/Lab0/Question1.m) needs to be run first to set variables.
+
+run(Question1)
+
+a = sin(x);
+b = exp(-y);
+c = power(b,2.0);
+d = sqrt(a);
+g = log(pi);
+h = log10(a);
+
+fprintf(1, 'a = %f \n',a);
+fprintf(1, 'b = %f \n',b);
+fprintf(1, 'c = %f \n',c);
+fprintf(1, 'd = %f \n',d);
+fprintf(1, 'g = %f \n',g);
+fprintf(1, 'h = %f \n',h);
