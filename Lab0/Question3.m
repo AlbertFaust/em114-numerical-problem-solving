5ea7516e999988fcf10ff33eab27d431c097716c
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-26 22:56:30 +0100

    Update Question3.m

--- Lab0/Question3.m
+++ Lab0/Question3.m
@@ -1,6 +1,6 @@
 %Question2.m (https://github.com/AlbertFaust/em114-numerical-problem-solving/blob/master/Lab0/Question2.m) needs to be run first to set variables.
 
-run(Question2)
+run Question2
 
 fprintf(1, 'b = %1.6f \n',b);
 fprintf(1, 'c = %1.6f \n',c);
