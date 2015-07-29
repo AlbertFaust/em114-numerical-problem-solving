f721c2ad092c70fee5c7c315ab9b85cf93d8178d
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-29 22:58:39 +0100

    Update if_example.m

--- Lab0/if_example.m
+++ Lab0/if_example.m
@@ -1,9 +1,9 @@
 counter = 0;
 satisfied = 0;
 while(satisfied == 0)
-if(counter * counter - 20 * counter - 30 > 500)
-fprintf(1,'Counter equals %d \n',counter);
-satisfied = 1;
-end
-counter = counter + 1;
+  if(counter * counter - 20 * counter - 30 > 500)
+    fprintf(1,'Counter equals %d \n',counter);
+    satisfied = 1;
+  end
+  counter = counter + 1;
 end
