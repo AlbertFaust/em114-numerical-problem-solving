e95645d4693de006e9bb5118f64edd41500b3e6e
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-31 10:15:04 +0100

    Update Question7.m

--- Lab0/Question7.m
+++ Lab0/Question7.m
@@ -1,7 +1,8 @@
 clear all;
 the_data = load('lab0_data.res');
 col_2 = the_data(:,2);
-sum = 0;
+S = 0;
 for ct = 1:1000
-  sum = sum + col_2(ct)
+  S = S + col_2(ct)
 end
+fprintf(1,'S equals %d\n',S);
