e6958e66a17ba99cc9ed743cb2f0cc3e6bc9f933
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-08-04 23:09:19 +0100

    Create Question1.m

---
+++ Lab1/GroupA/Question1.m
@@ -0,0 +1,20 @@
+clear all;
+
+N = 100;
+x_upper = 2*pi;
+x_lower = 0;
+delta_x = (x_upper-x_lower)/N ; 
+
+for ct=1:N
+  x(ct) = x_lower + (ct-0.5)*delta_x ;  
+  f1(ct) = cos(sqrt(x(ct)^4)+5);
+  f2(ct) = 0;
+end
+
+plot(x,f1,'r',x,f2,'b:')
+legend('f = cos(sqrt(x^4+5))','x-axis')
+xlabel('x')
+title('f(x) = cos(sqrt(x^4+5)) and x-axis')
+axis([0 2*pi -2 2])
+
+fprintf('f(x) crosses x-axis 12 times\n');
